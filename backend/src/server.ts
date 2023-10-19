c7db3104d2830af07e552b1badc7a34a57bdb2f6
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-19 19:23:36 +0200

    various fix and app separation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,20 @@
-import "dotenv/config"
-import env from "./util/validateEnv"
-import express from "express";
-import { PrismaClient } from '@prisma/client'
+import app from "./app";
+import env from "./util/validateEnv";
+import { PrismaClient } from '@prisma/client';
 
 
-const app = express();
 const prisma = new PrismaClient()
 const port = env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
-});
 
 
 async function main() {
   // const user = await prisma.user.create({ data: { name: "thomas"}})
   // console.log(user)
-  console.log("server running on port" + port);
 
+  app.listen(port, () => {
+    console.log("server running on port: " + port);
+  })
 }
 
 main()
