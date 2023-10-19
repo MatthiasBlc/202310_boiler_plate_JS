f47c56935ee139d9903e2a860853546fcfa2634e
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-19 19:06:59 +0200

    adding envalid for env validate

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,26 @@
 import "dotenv/config"
+import env from "./util/validateEnv"
 import express from "express";
+import { PrismaClient } from '@prisma/client'
 
 
 const app = express();
-const port = process.env.PORT;
+const prisma = new PrismaClient()
+const port = env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-app.listen(port, () => {
+
+async function main() {
+  // const user = await prisma.user.create({ data: { name: "thomas"}})
+  // console.log(user)
   console.log("server running on port" + port);
-});
+
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect())
+
