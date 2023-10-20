b43e5aeccbef22d9beddab6c44add8ca71e2093c
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-20 19:48:53 +0200

    adding error management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,36 @@
 import "dotenv/config"
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import prisma from './util/db'
 
 
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const notes = await prisma.note.findMany({
-    select: {
-      id: true,
-      title: true,
-    },
-  })
-  res.status(200).json(notes);
-  // res.send(notes);
-  // res.send("Hello World !");
+app.get("/", async (req, res, next) => {
+  try {
+    // throw Error("Crash Test");
+    const notes = await prisma.note.findMany({
+      select: {
+        id: true,
+        title: true,
+      },
+    })
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
 });
 
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
+});
 
 export default app;
