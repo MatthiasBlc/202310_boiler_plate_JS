996850180f5fc8eafa9bdb95bb7966faafa9503c
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-20 19:12:12 +0200

    update schema create seed and instantiate the db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import "dotenv/config"
 import express from "express";
+import prisma from './util/db'
+
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
+app.get("/", async (req, res) => {
+  const notes = await prisma.note.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  })
+  res.status(200).json(notes);
+  // res.send(notes);
+  // res.send("Hello World !");
 });
 
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,14 @@
 import app from "./app";
 import env from "./util/validateEnv";
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import prisma from './util/db'
 
-
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 const port = env.PORT;
 
 
 
 async function main() {
-  // const user = await prisma.user.create({ data: { name: "thomas"}})
-  // console.log(user)
-
   app.listen(port, () => {
     console.log("server running on port: " + port);
   })
