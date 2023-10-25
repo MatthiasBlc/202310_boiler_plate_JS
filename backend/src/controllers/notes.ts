68719bf153066ccee2854381f95beb4f331beed4
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-25 15:21:49 +0200

    adding single note get and post

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -15,5 +15,42 @@ export const getNotes: RequestHandler = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const getNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    const note = await prisma.note.findUnique({
+      where: {
+        id: noteId,
+      },
+      select: {
+        id: true,
+        title: true,
+      },
+    })
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
 }
 
+
+export const createNote: RequestHandler = async (req, res, next) => {
+  const title = req.body.title;
+  const text = req.body.text;
+
+  try {
+    const newNote = await prisma.note.create({
+      data: {
+        title: title,
+        text: text,
+      },
+    });
+    res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+
+};
