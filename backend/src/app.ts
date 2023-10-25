367620602721e9d186ef701c995c1fa655c45116
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-25 15:26:42 +0200

    adding morgan middleware for request management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
 import "dotenv/config"
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
-
+import morgan from "morgan";
 
 
 const app = express();
 
-app.use(express.json());
+app.use(morgan("dev"));
 
+app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
