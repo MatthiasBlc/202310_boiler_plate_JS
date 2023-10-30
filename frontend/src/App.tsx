14236c505bfb7ba96c9c77ec8e246a116414a254
MatthiasBlc <mat.bouloc@gmail.com>
2023-10-30 16:27:24 +0100

    installing DaisyUI Forest Theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,16 @@ function App() {
       <h1 className="text-3xl font-bold text-red-500 underline text-center">
         Hello world!
       </h1>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn w-64 rounded-full">Button</button>
+      <button className="btn btn-primary">One</button>
+      <button className="btn btn-secondary">Two</button>
+      <button className="btn btn-accent btn-outline">Three</button>
+
       <div className="App">
         {notes.map((note) => (
           <Note note={note} key={note.id} />
